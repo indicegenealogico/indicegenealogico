66a6d5c43e17ca83521ddd5dfedf8a503f7ddaa6
Indice Genealogico <indicegenealogico@gmail.com>
2022-08-05 12:47:49 -0600

    minimos

--- src/familytree/settings.py
+++ src/familytree/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os.path
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +30,7 @@ SECRET_KEY = 'django-insecure-d%i@nh^cvh7f*+k0b)gnz#r3s=1sib+k9+wgfdn__uw@nq7(!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['indicegenealogico.com', 'www.indicegenealogico.com']
+ALLOWED_HOSTS = ['indicegenealogico.com', 'www.indicegenealogico.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
